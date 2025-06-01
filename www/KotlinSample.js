8d9c3a5e65dd2335305949069459bed08bb3402b
siyaulhaqu <siya@headfitted.com>
2025-06-01 07:28:23 +0200

    Update KotlinSample.js

--- www/KotlinSample.js
+++ www/KotlinSample.js
@@ -2,9 +2,7 @@ var exec = require('cordova/exec');
 
 var KotlinSample = {
     echo: function(message, successCallback, errorCallback) {
-        alert("before");
         exec(successCallback, errorCallback, "KotlinSample", "echo", [message]);
-        alert("after");
     },
     getDate: function(successCallback, errorCallback) {
         exec(successCallback, errorCallback, "KotlinSample", "getDate", []);
